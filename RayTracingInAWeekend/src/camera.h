83c4a6c29109f745c25429de5254fc9b9aa792d6
Ljamo <masterluka21@gmail.com>
2024-01-12 21:18:22 +1100

    Added depth of field.

--- RayTracingInAWeekend/src/camera.h
+++ RayTracingInAWeekend/src/camera.h
@@ -27,6 +27,9 @@ public:
     point3 lookat = point3(0, 0, 0);
     vec3 vup = vec3(0, 1, 0);
 
+    double defocus_angle = 0;
+    double focus_dist = 10;
+
     void render(const hittable& world)
     {
         initialize();
@@ -105,12 +108,14 @@ public:
 
 
 private:
-    int    image_height;   // Rendered image height
-    point3 center;         // Camera center
-    point3 pixel00_loc;    // Location of pixel 0, 0
-    vec3   pixel_delta_u;  // Offset to pixel to the right
-    vec3   pixel_delta_v;  // Offset to pixel below
-    vec3   u, v, w;
+    int     image_height;   // Rendered image height
+    point3  center;         // Camera center
+    point3  pixel00_loc;    // Location of pixel 0, 0
+    vec3    pixel_delta_u;  // Offset to pixel to the right
+    vec3    pixel_delta_v;  // Offset to pixel below
+    vec3    u, v, w;
+    vec3    defocus_disk_u;
+    vec3    defocus_disk_v;
 
     void initialize()
     {
@@ -125,10 +130,9 @@ private:
 
         center = lookfrom;
 
-        auto focal_length = (lookfrom - lookat).length();
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta / 2);
-        auto viewport_height = 2 * h * focal_length;
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
         w = unit_vector(lookfrom - lookat);
@@ -142,20 +146,32 @@ private:
         pixel_delta_v = viewport_v / image_height;
 
         auto viewport_upper_left =
-            center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
+            center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
-    ray get_ray(int i, int j) const {
+    ray get_ray(int i, int j) const 
+    {
         // Get a randomly sampled camera ray for the pixel at location i,j.
+        // originates from defocus disk
 
         auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
         auto pixel_sample = pixel_center + pixel_sample_square();
 
-        auto ray_origin = center;
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return ray(ray_origin, ray_direction);
+    }
+
+    point3 defocus_disk_sample() const
+    {
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
     vec3 pixel_sample_square() const {
